b586a6f935f5549904a62698a0a94e540a9c0d87
Seulmi <kang.seulmi@gmail.com>
2015-07-31 13:51:22 +0900

    Create Powers of Two.rb

---
+++ Powers of Two.rb
@@ -0,0 +1,18 @@
+def PowersofTwo(num)
+
+  while num > 1 do
+    if(num % 2 != 0)
+      return "false"
+    end
+  
+    num /=2
+  end
+  
+  return "true"
+  
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+PowersofTwo(STDIN.gets) 
