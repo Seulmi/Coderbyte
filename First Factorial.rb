0657576922b7f0be13e7b1a41f5257e35a144cd0
Seulmi <kang.seulmi@gmail.com>
2015-07-31 13:39:14 +0900

    Create First Factorial.rb

---
+++ First Factorial.rb
@@ -0,0 +1,10 @@
+def FirstFactorial(num)
+
+  #num.downto(1)
+  return num.downto(1).inject(:*)
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)           
